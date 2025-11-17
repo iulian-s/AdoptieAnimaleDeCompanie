c68ef3fee8e4bdd41fa1e01650fd0da0a5215cca
iulian-s <juan@pc.Dlink>
2025-11-17 21:02:31 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/adoptie/AdoptieApplication.kt
@@ -0,0 +1,11 @@
+package com.example.adoptie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdoptieApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdoptieApplication>(*args)
+}
